92ee9fd1c644798b59c407e82277e53c3cb73edc
Luiz Henrique Guerra <lhguerra@gmail.com>
2020-03-03 23:43:29 -0300

    ownership and functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 fn main() {
-  let s1 = String::from("Hello");
-  let s2 = s1.clone();
+  let s = String::from("hello");
 
-  println!("{}, {}", s1, s2);
+  takes_ownership(s);
+
+  println!("{}", s);
+
+  let x = 5;
+
+  makes_copy(x);
+}
+
+fn takes_ownership(some_string: String) {
+  println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+  println!("{}", some_integer);
 }
