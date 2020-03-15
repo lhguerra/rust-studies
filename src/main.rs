c6a88fa0610f4a6f4c86c7fba952d222fd970a49
Luiz Henrique Guerra <lhguerra@gmail.com>
2020-03-15 12:55:18 -0300

    Slice type part 1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
-  let mut s = String::from("hello");
+  let result = first_word("hello beautiful".to_string());
 
-  change(&mut s);
+  println!("{}", result);
 }
 
-fn change(some_string: &mut String) {
-  some_string.push_str(", world");
+fn first_word(s: String) -> usize {
+  let bytes = s.as_bytes();
+
+  for (i, &item) in bytes.iter().enumerate() {
+    if item == b' ' {
+      return i;
+    }
+  }
+
+  s.len()
 }
