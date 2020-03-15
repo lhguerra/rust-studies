79ad6cc24fbcdf4df8754ced54b8a9ae6dd47e6d
Luiz Henrique Guerra <lhguerra@gmail.com>
2020-03-15 13:27:02 -0300

    Slice type part 3 - slices as parameters

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 fn main() {
-  let s = String::from("Hello beautiful");
+  let s = "Hello beautiful";
   let result = first_word(&s);
 
   println!("{}", result);
+
+  let s2 = String::from("Hello Beautiful");
+  let result2 = first_word(&s2[..]);
+
+  println!("{}", result2);
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
   let bytes = s.as_bytes();
 
   for (i, &item) in bytes.iter().enumerate() {
