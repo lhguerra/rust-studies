708024aeb0bc99cfd08b41487292be13f635f935
Luiz Henrique Guerra <lhguerra@gmail.com>
2020-03-15 13:22:38 -0300

    Slice type part 2

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 fn main() {
-  let result = first_word("hello beautiful".to_string());
+  let s = String::from("Hello beautiful");
+  let result = first_word(&s);
 
   println!("{}", result);
 }
 
-fn first_word(s: String) -> usize {
+fn first_word(s: &String) -> &str {
   let bytes = s.as_bytes();
 
   for (i, &item) in bytes.iter().enumerate() {
     if item == b' ' {
-      return i;
+      return &s[..i];
     }
   }
 
-  s.len()
+  &s[..]
 }
