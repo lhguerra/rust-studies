2506b86bf679d12b6abaf378a61f7314f22808a2
Luiz Henrique Guerra <lhguerra@gmail.com>
2020-04-19 17:22:48 -0300

    Printing struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
   width: u32,
   height: u32,
@@ -7,11 +8,7 @@ fn main() {
   let rect1 = Rectangle {width: 30, height: 50};
 
   println!(
-    "The area of the rectangle is {} square pixels",
-    area(&rect1)
+    "rect1 is {:?}",
+    rect1
   );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-  rectangle.width * rectangle.height
 }
